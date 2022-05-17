ead487a71e0d18c793b929f14ef6d8e698745600
Angele Williams <angele.williams5@gmail.com>
2022-05-17 16:28:44 -0600

    Refactor Router

--- src/App.js
+++ src/App.js
@@ -46,35 +46,6 @@ class App extends Component {
 
     )
   }
-    /*
-    if (!this.state.displaySingleMovie && !this.state.error){
-      return (
-        <main>
-          <Nav displayHome={this.displayHome}/>
-          <Movies onClick={this.displayMovie}
-            movies={this.state.movies}
-            displayMovie={this.displayMovie}
-          />
-        </main>
-    )}
-    else if (this.state.displaySingleMovie && !this.state.error) {
-      return (
-        <main>
-          <Nav displayHome={this.displayHome}/>
-          <MovieDetails
-          movie={this.state.currentMovie}
-          />
-        </main>
-      )
-    } else {
-      return(
-        <main>
-          <Nav displayHome={this.displayHome}/>
-          <h2 className="error-message">{this.state.error}</h2>
-        </main>
-      )
-    }*/
-
 
 }
 
@@ -82,3 +53,31 @@ export default App;
 
 
 //this.setState({displaySingleMovie: true, currentMovie: data.movie})
+/*
+if (!this.state.displaySingleMovie && !this.state.error){
+  return (
+    <main>
+      <Nav displayHome={this.displayHome}/>
+      <Movies onClick={this.displayMovie}
+        movies={this.state.movies}
+        displayMovie={this.displayMovie}
+      />
+    </main>
+)}
+else if (this.state.displaySingleMovie && !this.state.error) {
+  return (
+    <main>
+      <Nav displayHome={this.displayHome}/>
+      <MovieDetails
+      movie={this.state.currentMovie}
+      />
+    </main>
+  )
+} else {
+  return(
+    <main>
+      <Nav displayHome={this.displayHome}/>
+      <h2 className="error-message">{this.state.error}</h2>
+    </main>
+  )
+}*/
